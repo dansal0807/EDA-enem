4a5a7cd34eba02cb9d4cb5ef4fafce72e52fd586
dansal0807 <danielsaldanha0@gmail.com>
2021-10-21 17:56:29 -0300

    Formulação dos data frames.

--- data-analysis.py
+++ data-analysis.py
@@ -2,6 +2,7 @@ import pandas as pd
 from PyPDF2 import PdfFileReader
 import os
 import re
+import csv
 
 #Dados do SAS (http://sasedu.co/raioxenem2021):
 
@@ -13,9 +14,9 @@ topicos_filo = ["Ética e justiça", "Filosofia antiga", "Filosofia contemporân
 "Relações de poder", "Filosofia medieval", "Surgimento da Filosofia", "Intolerância"]
 
 topicos_socio = ["Mundo do trabalho", "Cultura e indústria cultural", "Ideologia", 
-"Meios de comunicação, tecnologia e cultura de massa", "Cidadania", "Movimentos sociais",
+"comunicação, tecnologia e cultura de massa", "Cidadania", "Movimentos sociais",
 "Identidade de gênero", "Desigualdades sociais", 
-"Organização científica do trabalho (taylorismo e fordismo)", "Conflito de terra e violência"]
+"Organização científica do trabalho", "Conflito de terra e violência"]
 
 quests_inteiras = ['64', '46', '41', '38', '28', '25', '23', '22', '19', '17', '13', '10', '9', '6', '29', '26', '19', '18', '18', '16', '12', '7', '5', '5', '34', '20', '18', 
 '18', '16', '14', '12', '11', '8', '4']
@@ -55,6 +56,36 @@ for i in range(len(topicos_hist) + len(topicos_filo) + 1, len(quests_inteiras)):
     quests_per_socio.append(quests_per[i])
 
 #criando o data frame:
+if not os.path.exists('datasets'):
+    os.mkdir('datasets')
+
+os.chdir('datasets')
+
+#primeiro data set feito só de tópicos
+dataset_topics = {'Tópicos História': topicos_hist, 'Tópicos Filosofia': topicos_filo, 'Tópicos Sociologia': topicos_socio}
+
+for i in range(len(topicos_hist)):
+    if len(topicos_filo) != len(topicos_hist):
+        topicos_filo += 'x'
+    
+    if len(topicos_socio) != len(topicos_hist):
+        topicos_socio += 'x'
+
+df_topics = pd.DataFrame(dataset_topics)
 
 
+with open('topicos ordenados.txt', 'w') as topicstxt:
+    topicstxt.write(str(df_topics))
 
+#Data set e porcentagens dos tópicos de história:
+
+for quests in quests_per_hist:
+    quests = str(quests) + '%'
+    quests_per_hist.append(quests)
+
+dataset_topics_hist = {'questões (numeros inteiros)': quests_inteiras_hist, 'questões (porcentagem)': quests_per_hist}
+
+df_hist = pd.DataFrame(dataset_topics_hist, index = topicos_hist)
+
+with open('estatisticas de história.txt', 'w') as topicstxt:
+    topicstxt.write(str(df_hist))
