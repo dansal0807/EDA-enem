feb82fca0ecdeec476cd14147aa22fd6720b7bbc
dansal0807 <danielsaldanha0@gmail.com>
2021-10-28 13:06:33 -0300

    Atualizações do data visualization.

--- data-visualization.py
+++ data-visualization.py
@@ -2,13 +2,14 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df_topics = pd.read_csv("datasets/tópicos ordenados.csv")
-print(df_topics)
+df_topics = pd.read_csv("datasets/tópicos ordenados.csv", index_col=False)
+topicos= df_topics.loc[:, ['Tópicos História', 'Tópicos Filosofia']]
+print(topicos)
+
 
 df_hist = pd.read_csv("datasets/tópicos de história.csv")
-df_hist = df_hist(index=False)
+df_hist_int = df_hist.loc[:, "questões (numeros inteiros)"]
+print(df_hist_int)
 
-sns.set_theme(style="whitegrid")
-ax  = sns.barplot(data=df_hist)
-plt.show()
+
 
