55bf518f96b0d914a5f8be57453211067ab530e2
dansal0807 <danielsaldanha0@gmail.com>
2021-11-02 19:29:14 -0300

    Aplicação das medidas de tempo para sociologia e filosofia.

--- time-schedule.py
+++ time-schedule.py
@@ -79,24 +79,50 @@ sns.set_style('darkgrid')
 sns.barplot(x=vals, y=keys)
 plt.show()
 
-##SEÇÃO DE HISTÓRIA:
-df_hist = pd.read_csv("datasets/tópicos de história.csv")
-topicos_hist = df_hist['Unnamed: 0'].values.tolist()
-questoes2_hist = df_hist['questões em %'].values.tolist()
+##SEÇÃO DE FILOSOFIA / SOCIOLOGIA:
+df_filo = pd.read_csv("datasets/tópicos de filosofia.csv")
+questoes2_filo = df_filo['questões em %'].values.tolist()
+topicos_filo = df_filo['Unnamed: 0'].values.tolist()
+
+df_socio = pd.read_csv("datasets/tópicos de sociologia.csv")
+questoes2_socio = df_socio['questões em %'].values.tolist()
+topicos_socio = df_socio['Unnamed: 0'].values.tolist()
+
+
 
 #aplicando a proporção dos dias e as matérias devidas:
-diasaplicados = []
-for porcentagem in questoes2_hist:
-    daysoftopics = (43*round(porcentagem, 0))/100
+##Aqui existe uma pequena diferença em relação a aplicação de historia; como segunda-feira é um dia tanto para filosofia como sociologia, resolvo dividir pela metade os dias para cada disciplina.
+##Dessa forma, divido 22 dias para cada um, deixando 1 dia para revisão de ambas as matérias.
+diasaplicados_filo = []
+for porcentagem in questoes2_filo:
+    daysoftopics = (22*round(porcentagem, 0))/100
     daysoftopics = round(daysoftopics, 0)
     diasaplicados.append(daysoftopics)
 
-#dicionário com as proporções devidas: tópicos por porcentagem aplicada a quantidade total de dias (terça-feira)
-proportion_hist = {topicos_hist[i]: diasaplicados[i] for i in range(len(topicos_hist))}
+#dicionário com as proporções devidas: tópicos por porcentagem aplicada aos 22 dias.
+proportion_filo = {topicos_filo[i]: diasaplicados[i] for i in range(len(topicos_filo))}
 
-#transformando em uma visualização:
-keys = list(proportion_hist.keys())
-vals = list(proportion_hist.values())
+#Aplicando para sociologia:
+diasaplicados_socio = []
+for porcentagem in questoes2_socio:
+    daysoftopics = (22*round(porcentagem, 0))/100
+    daysoftopics = round(daysoftopics, 0)
+    diasaplicados.append(daysoftopics)
+
+#dicionário com as proporções devidas: tópicos por porcentagem aplicada aos 22 dias.
+proportion_socio = {topicos_socio[i]: diasaplicados[i] for i in range(len(topicos_socio))}
+
+#transformando em uma visualização (filo):
+keys = list(proportion_filo.keys())
+vals = list(proportion_filo.values())
 sns.set_style('darkgrid')
 sns.barplot(x=vals, y=keys)
 plt.show()
+
+#transformando em uma visualização (socio):
+keys = list(proportion_socio.keys())
+vals = list(proportion_socio.values())
+sns.set_style('darkgrid')
+sns.barplot(x=vals, y=keys)
+plt.show()
+
