92a54a41a51ac5f0ad28f914e7a74e7f1dafb7f7
Daniel Soares Saldanha <danielsaldanha0@gmail.com>
2021-12-21 15:00:36 -0300

    Update data-model.py

--- data-model.py
+++ data-model.py
@@ -16,6 +16,8 @@ import matplotlib.pyplot as plt
 
 import pandas as pd
 
+#Ainda incompleto 21/12
+
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = pd.read_csv(url, names=names)
